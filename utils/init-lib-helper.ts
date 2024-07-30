d4bdaf0bdad9c5fe2de9d5ca3021bbdcb47fddb0
julia kharlamova <juliettkhar@gmail.com>
2024-07-30 09:00:21 +0300

    update

--- utils/init-lib-helper.ts
+++ utils/init-lib-helper.ts
@@ -1,27 +1,32 @@
-import { Reviewer } from 'reviewer-lib';
-require('dotenv').config();
+// @ts-ignore
+import {Reviewer} from '../dist';
 
 const apiKey = process.env.OPENAI_API_KEY;
 
 if (!apiKey) {
-  console.error('Error: OPENAI_API_KEY is not set');
-  process.exit(1);
+    console.error('Error: OPENAI_API_KEY is not set');
+    process.exit(1);
 }
 
 const reviewer = new Reviewer(apiKey);
 let code = '';
 
-process.stdin.on('data', chunk => {
-  code += chunk;
+process.stdin.on('data', () => {
+    code += `
+function exampleFunction(x, y) {
+  let result = x + y;
+  return result;
+}
+`;
+    console.info(process.env.OPENAI_API_KEY)
 });
 process.stdin.on('end', () => {
-        reviewer.submitCode(code)
-            .then((feedback: string | undefined) => {
-                console.log('Code Review Feedback:');
-                console.log(feedback);
-            })
-            .catch((error: Error | string) => {
-                console.error('Error:', error);
-                process.exit(1);
-            })
+    reviewer.submitCode(code)
+        .then((feedback: string | undefined) => {
+            console.log('Code Review Feedback:', feedback);
+        })
+        .catch((error: Error | string) => {
+            console.error('Error:', error);
+            process.exit(1);
+        })
 });
